464006ba21d15e2e2fa03b0ceca6d1a2e31a305d
Igor <kipr29611@gmail.com>
2022-12-01 15:17:17 +0300

    new_code2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int user_imput1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int user_imput2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int user_imput3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное число");
+Console.WriteLine(Math.Max(Math.Max(user_imput1,user_imput2),user_imput3));
