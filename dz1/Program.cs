33bdd16d6ee1696c379a55af70ce6dcb853b2b93
Igor <kipr29611@gmail.com>
2022-12-01 15:01:38 +0300

    new_code1

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int user_imput1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int user_imput2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное число");
+Console.WriteLine(Math.Max(user_imput1,user_imput2));
+Console.WriteLine("Минимальное число");
+Console.WriteLine(Math.Min(user_imput1,user_imput2));
