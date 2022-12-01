f946a105dbdf7efbe748e81961426958098480ba
Igor <kipr29611@gmail.com>
2022-12-01 16:27:22 +0300

    new_3_code

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите число:");
+int input = Convert.ToInt32(Console.ReadLine());
+        if ((input % 2) == 0)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
