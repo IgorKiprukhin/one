5f642a40d4d9ee8a377e675dc4c8f8ed79eb1cea
Igor <kipr29611@gmail.com>
2022-12-06 11:07:34 +0300

    new_4_code

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿        int n = 1;
+        Console.Write("Введите число: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
